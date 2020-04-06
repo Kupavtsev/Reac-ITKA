6dd40cbd3d5ca2df58f3b77b776adb3031b5e1e4
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-04-06 15:52:48 +0300

    create github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// 16 watch many times
+// 19
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
@@ -7,6 +7,9 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
@@ -18,6 +21,9 @@ const App = () => {
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs' component={Dialogs}/>
                     <Route path='/profile' component={Profile}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
     return (
-      <div>Main Content
+      <div>
         <div>
           <img src='https://img.freepik.com/free-vector/abstract-circle-dot-gradation-background_2095-224.jpg?size=626&ext=jpg'></img>
         </div>
