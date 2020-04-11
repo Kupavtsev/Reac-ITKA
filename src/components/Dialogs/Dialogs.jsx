c3d3149f01112ae3b5bba0d828df72489f1fe9ac
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-04-11 18:50:02 +0300

    21 dialogs grid

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Oleg
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Yuri
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your mood ?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
