c0b8d12d0c57167079e3b125cf365f5cacb07d2f
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-04-15 17:20:29 +0300

    29 partial 16:49 until its work

--- src/index.js
+++ src/index.js
@@ -3,40 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import state from './redux/state';
 
-let posts =
-    [
-        {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 27},
-        {id: 3, message: 'Are you going to trade?', likesCount: 127},
-        {id: 4, message: 'What about Fib levels?', likesCount: 327},
-    ]
 
-let dialogs =
-    [
-        {id: 1, name: 'Oleg'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Yuri'},
-        {id: 5, name: 'Dima'},
-        {id: 6, name: 'Sasha'}
-    ];
+ReactDOM.render(<App state={state} />, document.getElementById('root'));
 
-let messages =
-    [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your mood ?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'},
-    ];
-
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <App posts={posts} dialogs={dialogs} messages={messages}/>
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/redux/state.js
@@ -0,0 +1,31 @@
+
+
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 12},
+            {id: 2, message: 'It\'s my first post', likesCount: 27},
+            {id: 3, message: 'Are you going to trade?', likesCount: 127},
+            {id: 4, message: 'What about Fib levels?', likesCount: 327},
+        ]
+    },
+    messagesPage: {
+        dialogs: [
+            {id: 1, name: 'Oleg'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Yuri'},
+            {id: 5, name: 'Dima'},
+            {id: 6, name: 'Sasha'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your mood ?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'},
+        ]
+    }
+};
+
+export default state;
