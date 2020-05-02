cd4efeede70b39c1291acf5c86c2b1c20dfccf18
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-05-02 15:57:09 +0300

    41 reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,10 +1,8 @@
 // This is exact copy of REDUX store
 // To make it clear, how Redux work inside
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
-const SEND_MESSAGE = 'SEND_MESSAGE';
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
 
 let store = {
     _state: {
@@ -35,7 +33,7 @@ let store = {
             ],
             newMessageBody: ""
         },
-        sitebar: {}
+        sidebar: {}
     },
     _callSubscriber () {
         console.log('State has changed');
@@ -60,39 +58,17 @@ let store = {
     // it's very similiar with 'publisher-subscriber', 'addEventListener'
 
     dispatch(action) {
-        if (action.type === ADD_POST) {
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = '';
-            this._callSubscriber(this._state);
-        } else if (action.type === UPDATE_NEW_POST_TEXT) {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state);
-        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-            this._state.dialogsPage.newMessageBody = action.body;
-            this._callSubscriber(this._state);
-        } else if (action.type === SEND_MESSAGE) {
-            let body = this._state.dialogsPage.newMessageBody;
-            this._state.dialogsPage.newMessageBody = '';
-            this._state.dialogsPage.messages.push( { id: 6, message: body } );
-            this._callSubscriber(this._state);
-        }
-
+        // Это превращается в новый profilePage после обработки Редьюсером
+        // Т.е. через эту переменную state обновился
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+        // Мы сначала преоброзавали ветки store редьюсором
+        // а дольше уведомили подписчиков
+        this._callSubscriber(this._state);
     }
 
 };
-
-export const addPostActionCreator = () => ({ type: ADD_POST });
-export const updateNewPostTextActionCreator = (text) =>
-    ({ type: UPDATE_NEW_POST_TEXT, newText: text });
-
-export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
-export const updateNewMessageBodyCreator = (body) =>
-    ({ type: UPDATE_NEW_MESSAGE_BODY, body: body });
 
 
 export default store;
