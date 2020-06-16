6969e9df4e4028d12cbba6ee897e2ea9edb0971a
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-06-16 17:25:36 +0300

    59 profile page, ajax, api

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    /*if (props.profile == null || typeOf(props.profile) == undefined)*/
+    if (!props.profile) {
+        return <Preloader />
+    }
+
     return (
         <div>
             <div className={s.image}>
@@ -9,8 +15,10 @@ const ProfileInfo = () => {
                     src='https://miro.medium.com/max/2800/1*WbcprlGOq8O5qVs7l-tPww.png'></img>
             </div>
             <div className={s.descriptionBlock}>
-
+                <img src={props.profile.photos.large} />
             </div>
+            <div>{props.profile.aboutMe}</div>
+
         </div>
     )
 }
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     posts: [
@@ -8,7 +9,8 @@ let initialState = {
         {id: 3, message: 'Are you going to trade?', likesCount: 127},
         {id: 4, message: 'What about Fib levels?', likesCount: 327},
     ],
-    newPostText: 'Click add post, to initiate CallBack which will dispatch action creator throw reducer to state...'
+    newPostText: 'Click add post, to initiate CallBack which will dispatch action creator throw reducer to state...',
+    profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -32,6 +34,9 @@ const profileReducer = (state = initialState, action) => {
             };
         }
         // Если необходимого action не придет, то вернется state по умолчанию
+        case SET_USER_PROFILE: {
+            return {...state, profile: action.profile}
+        }
         default:
             return state;
     }
@@ -40,5 +45,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export const updateNewPostTextActionCreator = (text) =>
     ({ type: UPDATE_NEW_POST_TEXT, newText: text });
+// SET_USER_PROFILE название действия, profile сидит в Action
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 
 export default profileReducer;
