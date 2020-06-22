488d930aeb95a62db02448c5e4f5453d821fc85a
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-06-22 19:41:32 +0300

    62 follow, from 20min UsersContainer.jsx not done

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./users.module.css";
 import userPhoto from '../../assets/images/user.png';
 import {NavLink} from "react-router-dom";
+import * as axios from 'axios';
 
 let Users = (props) => {
 
@@ -41,10 +42,40 @@ let Users = (props) => {
                         {/*Разные кнопки, в зависимоти от followed/unfollowed*/}
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+
+                                /*On the DELETE withCredentials second Arg*/
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "free"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.unfollow(u.id);
+                                        }
+                                    });
+
+
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+
+                                /* Server API follow/{userId}, we must be authorized
+                                * Our second Arg is null or an empty object*/
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY": "free"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.follow(u.id);
+                                        }
+                                    });
+
+
+
                             }}>Follow</button>}
                     </div>
                 </span>
