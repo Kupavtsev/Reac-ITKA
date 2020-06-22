50254327c019ef8e0643e42f15ae6e807aee953e
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-06-22 23:28:24 +0300

    63 DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+import * as axios from 'axios';
+
+
+const instance =  axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "d53023ab-c51d-4d61-b36a-47160fb42243"
+    }
+});
+
+export const userAPI = {
+    getUsers (currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            });
+    }
+};
+
+export const getUsers = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        });
+};
+
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        });
+};
