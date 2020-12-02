d838b2ab0f9d01990fd871f1c4c57989342a9255
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-12-02 15:25:20 +0300

    Updates

---
+++ precache-manifest.ca54c449734bf4223ef61eccd1bf5ce9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726d2681df2d7a2317808706aea503ec",
+    "url": "/Reac-ITKA/index.html"
+  },
+  {
+    "revision": "6295aff3e054a37d886a",
+    "url": "/Reac-ITKA/static/css/3.33c873d1.chunk.css"
+  },
+  {
+    "revision": "b627cd8e351cb310fdd0",
+    "url": "/Reac-ITKA/static/css/4.94e8bc57.chunk.css"
+  },
+  {
+    "revision": "eeff61ddabfc89affd1c",
+    "url": "/Reac-ITKA/static/css/main.cba8ab94.chunk.css"
+  },
+  {
+    "revision": "1db37ce41f691874399c",
+    "url": "/Reac-ITKA/static/js/2.763ea104.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Reac-ITKA/static/js/2.763ea104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6295aff3e054a37d886a",
+    "url": "/Reac-ITKA/static/js/3.6ceece01.chunk.js"
+  },
+  {
+    "revision": "b627cd8e351cb310fdd0",
+    "url": "/Reac-ITKA/static/js/4.da3dd7e5.chunk.js"
+  },
+  {
+    "revision": "eeff61ddabfc89affd1c",
+    "url": "/Reac-ITKA/static/js/main.2097366d.chunk.js"
+  },
+  {
+    "revision": "abaea550dd0971f82168",
+    "url": "/Reac-ITKA/static/js/runtime-main.18a92d53.js"
+  },
+  {
+    "revision": "c9e96b5e147f94a8072fc6abd83884ac",
+    "url": "/Reac-ITKA/static/media/4.c9e96b5e.svg"
+  }
+]);
